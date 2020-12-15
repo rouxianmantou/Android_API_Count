36c35ba84bf0bd7c0575293c92b388280285da85
rxmt <ghlvahe@tju.edu.cn>
2020-12-15 16:22:14 +0800

    add clone_and_count_lib_in_target.sh to clone git repo 2

---
+++ clone_and_count_lib_in_target.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#请参考clone_lib_list的sample list
+#e.g. gyf-dev/ImmersionBar 
+
+abs_tar_path="/target"
+abs_result_path="/result"
+
+abs_list_path="/clone_lib_list"
+result_path=`pwd`$abs_result_path
+
+lib_path=`pwd`$abs_tar_path
+clone_lib_list_path=`pwd`$abs_list_path
+
+cd target
+echo "" > $lib_path/git_clone_info.log
+download_index=0
+
+cat $clone_lib_list_path | awk '{print $0}' | while read line
+do 
+    download_index=`expr $download_index + 1`
+    echo $download_index >> $lib_path/git_clone_info.log
+    echo "START CLONE -> $line"
+    echo "$line" >> $lib_path/git_clone_info.log
+
+
+    if ! `git clone https://github.com/"$line" --progress --depth 1 --single-branch`
+    then
+      echo "FAILED!!!!!!" >> $lib_path/git_clone_info.log
+    else
+      echo "OK!" >> $lib_path/git_clone_info.log
+    fi
+    echo "************************************************************************************************************************" >> git_clone_info.log
+    echo "" >> git_clone_info.log
+    echo "DONE CLONE -> $line"
+    echo "************************************************************************************************************************"
+done
+cd ..
+
+chmod +x batch_count.sh
+./batch_count.sh
